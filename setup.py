ae24b2fcd0873ef5cecc712be669e262c52e895a
Mikko Kotila <mailme@mikkokotila.com>
2023-07-29 14:15:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,26 +4,15 @@ from codecs import open
 from os import path
 from setuptools import setup, find_packages
 
-root = path.abspath(path.dirname(__file__))
-
-with open(path.join(root, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-with open(path.join(root, 'LICENSE.txt'), encoding='utf-8') as f:
-    license = f.read()
-
-with open(path.join(root, 'VERSION.txt'), encoding='utf-8') as f:
-    version = f.read().strip()
-
 setup(
     name='Bokit',
-    version=version,
+    version=v0.2,
     description='Bokit is a Python API that exposes commonly used tools for various Tibetan language workflows.',
-    long_description=long_description,
+    long_description='Bokit is a Python API that exposes commonly used tools for various Tibetan language workflows.',
     url='https://github.com/lopenling/bokit',
     author='Mikko Kotila',
     author_email='mailme@mikkokotila.com',
-    license=license,
+    license='MIT',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -37,8 +26,5 @@ setup(
     packages=find_packages(),
     extras_require={
         '': ['pytest'],
-    },
-    package_data={
-        '': ['VERSION.txt', 'LICENSE.txt']
     },
 )
